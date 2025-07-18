cc10ece1b138a26ddb02ec649a07421e1d56eb60
Asad06124 <asadbalqani@gmail.com>
2025-07-18 15:26:53 +0500

    Add comment to main.dart for clarity

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:roomrounds/helpers/data_storage_helper.dart';
 import 'package:roomrounds/module/profile/controller/profile_controller.dart';
 
 import 'firebase_options.dart';
+// main.dart
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
